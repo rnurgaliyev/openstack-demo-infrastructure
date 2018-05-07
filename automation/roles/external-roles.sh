88e7b8c94926ec8177ab14f0a6d6e81b6f938a4e
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2018-05-07 10:53:22 +0200

    Rewoork external roles

---
+++ automation/roles/external-roles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e 
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Remove existing roles
+rm -fR $SCRIPT_DIR/external
+
+# Create external role directory
+mkdir -p $SCRIPT_DIR/external
+
+# Run ansible-galaxy
+ansible-galaxy install -r $SCRIPT_DIR/external-roles.yml -p $SCRIPT_DIR/external
+
