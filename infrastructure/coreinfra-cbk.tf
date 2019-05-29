9438be75253e127b84b3300c390b4789f9cf18dc
imple <impleman@gmail.com>
2019-05-29 16:26:44 +0200

    Terraform 0.12

--- infrastructure/coreinfra-cbk.tf
+++ infrastructure/coreinfra-cbk.tf
@@ -1,24 +1,15 @@
 # Base Modules
 
 module "base_configuration_cbk" {
-  source = "modules/base-configuration"
+  source = "./modules/base-configuration"
 
   providers = {
     "openstack" = "openstack.cbk"
   }
 }
 
-# Base images are now provided by vendor
-# module "base_images_cbk" {
-#   source = "modules/base-images"
-
-#   providers = {
-#     "openstack" = "openstack.cbk"
-#   }
-# }
-
 module "base_networking_cbk" {
-  source          = "modules/base-networking"
+  source          = "./modules/base-networking"
   live_network    = "10.201.1.0/24"
   staging_network = "10.201.2.0/24"
 
